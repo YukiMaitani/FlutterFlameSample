5c969e33a7adc5d929c47053a7e7bcb6de0b8382
YukiMaitani <fightzoi1997@gmail.com>
2023-07-27 20:42:01 +0900

    ブロックを追加

--- lib/game/block_breaker.dart
+++ lib/game/block_breaker.dart
@@ -2,6 +2,7 @@ import 'package:flame/game.dart';
 
 import '../../constants/constants.dart';
 import 'component/ball.dart';
+import 'component/block.dart';
 import 'component/paddle.dart';
 
 class BlockBreaker extends FlameGame {
@@ -16,8 +17,9 @@ class BlockBreaker extends FlameGame {
     await addAll([
       paddle,
     ]);
-    
+
     resetBall();
+    resetBlock();
   }
 
   Future<void> resetBall() async {
@@ -28,5 +30,33 @@ class BlockBreaker extends FlameGame {
       ..y = size.y * kBallStartYRatio;
 
     await add(ball);
+  }
+
+  Future<void> resetBlock() async {
+    final sizeX = (size.x -
+            kBlocksStartXPosition * 2 -
+            kBlockPadding * (kBlocksRowCount - 1)) /
+        kBlocksRowCount;
+
+    final sizeY = (size.y * kBlocksHeightRatio -
+            kBlocksStartYPosition -
+            kBlockPadding * (kBlocksColumnCount - 1)) /
+        kBlocksColumnCount;
+    final blocks =
+        List<Block>.generate(kBlocksColumnCount * kBlocksRowCount, (int index) {
+      final block = Block(
+        blockSize: Vector2(sizeX, sizeY),
+      );
+      final indexX = index % kBlocksRowCount;
+      final indexY = index ~/ kBlocksRowCount;
+
+      block.position
+        ..x = kBlocksStartXPosition + indexX * (block.size.x + kBlockPadding)
+        ..y = kBlocksStartYPosition + indexY * (block.size.y + kBlockPadding);
+
+      return block;
+    });
+
+    await addAll(blocks);
   }
 }

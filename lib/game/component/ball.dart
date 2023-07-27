91bda73338b3ba089af1b6cd3b092c4370fbaa2e
YukiMaitani <fightzoi1997@gmail.com>
2023-07-28 08:13:06 +0900

    ボールが動く

--- lib/game/component/ball.dart
+++ lib/game/component/ball.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 
@@ -8,5 +9,23 @@ class Ball extends CircleComponent {
   Ball() {
     radius = kBallRadius;
     paint = Paint()..color = kBallColor;
+
+    final vx = kBallSpeed * cos(spawnAngle * kRad);
+    final vy = kBallSpeed * sin(spawnAngle * kRad);
+    velocity = Vector2(vx, vy);
+  }
+  late Vector2 velocity;
+
+  double get spawnAngle {
+    final random = Random().nextDouble();
+    final spawnAngle =
+    lerpDouble(kBallMinSpawnAngle, kBallMaxSpawnAngle, random)!;
+    return spawnAngle;
+  }
+
+  @override
+  void update(double dt) {
+    position += velocity * dt;
+    super.update(dt);
   }
 }
